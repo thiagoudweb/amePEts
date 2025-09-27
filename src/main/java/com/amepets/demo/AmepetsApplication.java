7ab8c3e87c08a024708d23946ba66a44c72fd455
Thiago Silva <thhzmonster@gmail.com>
2025-09-27 02:51:44 -0300

    initial commit

---
+++ src/main/java/com/amepets/demo/AmepetsApplication.java
@@ -0,0 +1,13 @@
+package com.amepets.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmepetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmepetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amepets/demo/AmepetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amepets.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmepetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
